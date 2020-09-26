f4445120d33b4744e31942709463784741b29dd4
Alexa-Joanne Gomez <alexagomez@Alexa-Joannes-MacBook-Pro-2.local>
2020-09-26 14:45:58 -0400

    google cloud?

--- ramhacks-app/src/environments/environment.prod.ts
+++ ramhacks-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB77B0ixl6n_5H4LWtDZmdWIbGpp1lOFzs",
+    authDomain: "ramhacks-cloud.firebaseapp.com",
+    databaseURL: "https://ramhacks-cloud.firebaseio.com",
+    projectId: "ramhacks-cloud",
+    storageBucket: "ramhacks-cloud.appspot.com",
+    messagingSenderId: "185373263088",
+    appId: "1:185373263088:web:63ffbac59d59299b82ee35",
+    measurementId: "G-3BWVE3DQH0"
+  }
 };
